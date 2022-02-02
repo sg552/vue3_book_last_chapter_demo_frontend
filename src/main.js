1c1bd12dcb757abfa6198db5519c29025d5b4a1b
申思维 <shensiwei@sina.com>
2022-02-02 15:25:45 +0800

    修改了src/main.js

--- src/main.js
+++ src/main.js
@@ -1,35 +1,17 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './main.vue'
+const app = createApp(App)
+
+// 这个是 远程接口的前缀。
+// 完整的接口地址形如： http://shopweb.sweetysoft.com/interface/goods/get_goods
+app.config.globalProperties.api = 'http://shopweb.sweetysoft.com/interface/'
+
+/*
+import store from './vuex/store'
+app.use(store)
+*/
+
 import router from './router'
-import VueResource from 'vue-resource'
-import vueConfig from './vue-config'
-import { currency } from './currency'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import VeeValidate from 'vee-validate'
+app.use(router)
+  .mount('#app')
 
-
-Vue.filter('currency', currency)
-
-Vue.config.productionTip = false
-Vue.use(VueResource)
-Vue.http.options.emulateJSON = true;
-
-const configs = {
-  // 这个是 远程接口的前缀。
-  // 完整的接口地址形如： http://shopweb.sweetysoft.com/interface/goods/get_goods
-  api: 'http://shopweb.sweetysoft.com/interface/'
-}
-
-Vue.use(vueConfig, configs)
-Vue.use(VueAwesomeSwiper)
-Vue.use(VeeValidate)
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
