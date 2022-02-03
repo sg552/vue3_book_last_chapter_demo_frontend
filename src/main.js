f355559db3d71fb666fe15f95afbbc460b05d303
申思维 <shensiwei@sina.com>
2022-02-03 15:01:33 +0800

    增加了一点注释( currency.js )

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ const app = createApp(App)
 // 完整的接口地址形如： http://shopweb.sweetysoft.com/interface/goods/get_goods
 app.config.globalProperties.api = 'http://shopweb.sweetysoft.com/interface'
 
+
+// 允许所有的.vue页面调用 currency 这个方法
 import { currency } from '@/currency.js'
 app.config.globalProperties.currency = currency
 
